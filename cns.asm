268209c4f5942984140a2170fc7033e5891d38ac
AhmadShykh <52326090+AhmadShykh@users.noreply.github.com>
2022-10-31 18:36:34 +0500

    Beach Check it Out

--- cns.asm
+++ cns.asm
@@ -4,18 +4,26 @@ score:      db      'Score: '
 strlenScore:    dw  7
 time:       db      'Time: '
 strlenTime: dw      6
-welcomeMess:    db  'Welcome!'
-strlenWelcomeMess:  dw  8
-cnsMess:    db  'Catch & Carry'
-strlenCnsMess:  dw  13
+welcomeMess:    db  'W E L C O M E !'
+strlenWelcomeMess:  dw  15
+cnsMess:    db  'C A T C H  &  C A R R Y'
+strlenCnsMess:  dw  23
 enterMess:  db  'Press Enter to Continue'
 strlenEnterMess:    dw  23
-instrucMess:    db  'Instructions'
-strlenInstrucMess:  dw  12
-instructions:   db  'blah blah'
-strlenInstructionsMess: dw  9
+instrucMess:    db  'Instructions :'
+strlenInstrucMess:  dw  14
 endMessage:     db  'Thank you for playing!'
 strlenEndMessage:   dw  25
+deadMsg:     db  'Y O U  D I E '
+deadMsgLength:   dw  13
+maxPointMsg:     db  '15 Points '
+maxMsgLength:   dw  10
+midPointMsg:     db  '10 Points '
+midMsgLength:   dw  10
+minPointMsg:     db  '5 Points'
+minMsgLength:   dw  8
+pointMsg:     db  '>'
+pointMsgLength:   dw  1
 
 
 clearScreen:
@@ -186,7 +194,7 @@ MainMenu:
     push    ax
     call    printText
     
-    mov     ax,     2Dh   ;x co-ordinate
+    mov     ax,     2h   ;x co-ordinate
     push    ax
     mov     ax,     0Dh   ;y co-ordinate
     push    ax
@@ -194,22 +202,127 @@ MainMenu:
     push    ax
     push    word[strlenInstrucMess]
     xor     ax,     ax
-    mov     ah,     0xC0
-    push    ax
-    call    printText
-
-    mov     ax,     2Dh   ;x co-ordinate
-    push    ax
-    mov     ax,     0Fh   ;y co-ordinate
-    push    ax
-    mov     ax,     instructions
-    push    ax
-    push    word[strlenInstructionsMess]
-    xor     ax,     ax
-    mov     ah,     30h
+    mov     ah,     0x37
     push    ax
     call    printText
     
+    ;Printing MAX Point Instruction
+
+    mov     al,14
+    mov     bl,2
+    call    maxPointShape
+    
+    mov     ax,     7   ;x co-ordinate
+    push    ax
+    mov     ax,     0Fh   ;y co-ordinate
+    push    ax
+    mov     ax,     pointMsg
+    push    ax
+    push    word[pointMsgLength]
+    xor     ax,     ax
+    mov     ah,     0x72
+    push    ax
+    call    printText
+    mov     ax,     0xA   ;x co-ordinate
+    push    ax
+    mov     ax,     0Fh   ;y co-ordinate
+    push    ax
+    mov     ax,     maxPointMsg
+    push    ax
+    push    word[maxMsgLength]
+    xor     ax,     ax
+    mov     ah,     0x72
+    push    ax
+    call    printText
+
+    ;Printing MID Point Instruction
+
+    mov     al,18
+    mov     bl,2
+    call    midPointShape
+    
+    mov     ax,     7   ;x co-ordinate
+    push    ax
+    mov     ax,     13h   ;y co-ordinate
+    push    ax
+    mov     ax,     pointMsg
+    push    ax
+    push    word[pointMsgLength]
+    xor     ax,     ax
+    mov     ah,     0x31
+    push    ax
+    call    printText
+    mov     ax,     0xA   ;x co-ordinate
+    push    ax
+    mov     ax,     13h   ;y co-ordinate
+    push    ax
+    mov     ax,     midPointMsg
+    push    ax
+    push    word[midMsgLength]
+    xor     ax,     ax
+    mov     ah,     0x31
+    push    ax
+    call    printText
+
+    ;Printing MIN Point Instruction
+
+    mov     al,22
+    mov     bl,2
+    call    minPointShape
+    
+    mov     ax,     7   ;x co-ordinate
+    push    ax
+    mov     ax,     17h   ;y co-ordinate
+    push    ax
+    mov     ax,     pointMsg
+    push    ax
+    push    word[pointMsgLength]
+    xor     ax,     ax
+    mov     ah,     0x36
+    push    ax
+    call    printText
+    mov     ax,     0xA   ;x co-ordinate
+    push    ax
+    mov     ax,     17h   ;y co-ordinate
+    push    ax
+    mov     ax,     minPointMsg
+    push    ax
+    push    word[minMsgLength]
+    xor     ax,     ax
+    mov     ah,     0x36
+    push    ax
+    call    printText
+
+    ;Printing DEAD Point Instruction
+
+    mov     al,14
+    mov     bl,25
+    call    holdMyTnt
+    
+    mov     ax,     1Eh   ;x co-ordinate
+    push    ax
+    mov     ax,     0Fh   ;y co-ordinate
+    push    ax
+    mov     ax,     pointMsg
+    push    ax
+    push    word[pointMsgLength]
+    xor     ax,     ax
+    mov     ah,     0x34
+    push    ax
+    call    printText
+    mov     ax,     20h   ;x co-ordinate
+    push    ax
+    mov     ax,     0Fh   ;y co-ordinate
+    push    ax
+    mov     ax,     deadMsg
+    push    ax
+    push    word[deadMsgLength]
+    xor     ax,     ax
+    mov     ah,     0x34
+    push    ax
+    call    printText
+
+
     ret
 
 holdMyTnt: 
@@ -247,6 +360,138 @@ holdMyTnt:
     mov     ax,0x34CA
     STOSw 
     mov     ax,0x34CD
+    STOSw
+
+    pop     cx
+    pop     es
+    pop     di
+
+ret 
+
+maxPointShape:
+    
+
+    push    di
+    push    es
+    push    cx    
+
+    mov     cx,80
+    mov     ah,0
+    mul     cx
+    mov     bh,0
+    add     ax,bx
+    shl     ax,1
+    mov     di,ax           ; selecting location on screen
+    mov     ax,0xB800
+    mov     es,ax
+    CLD
+    mov     ax,0x72C9       ; just printing shape afterwards
+    STOSw 
+    mov     ax,0x72B0
+    STOSw 
+    mov     ax,0x72BB
+    STOSw 
+    add     di,154
+    mov     ax,0x72B0
+    STOSw 
+    mov     ax,0x72B2
+    STOSw 
+    mov     ax,0x72B0
+    STOSw 
+    add     di,154
+    mov     ax,0x72C8
+    STOSw
+    mov     ax,0x72C9
+    STOSw 
+    mov     ax,0x72BC
+    STOSw
+
+    pop     cx
+    pop     es
+    pop     di
+
+ret 
+
+midPointShape:
+    
+
+    push    di
+    push    es
+    push    cx    
+
+    mov     cx,80
+    mov     ah,0
+    mul     cx
+    mov     bh,0
+    add     ax,bx
+    shl     ax,1
+    mov     di,ax           ; selecting location on screen
+    mov     ax,0xB800
+    mov     es,ax
+    CLD
+    mov     ax,0x31C9       ; just printing shape afterwards
+    STOSw 
+    mov     ax,0x31B0
+    STOSw 
+    mov     ax,0x31BB
+    STOSw 
+    add     di,154
+    mov     ax,0x31B0
+    STOSw 
+    mov     ax,0x31B2
+    STOSw 
+    mov     ax,0x31B0
+    STOSw 
+    add     di,154
+    mov     ax,0x31C8
+    STOSw
+    mov     ax,0x31CD
+    STOSw 
+    mov     ax,0x31BC
+    STOSw
+
+    pop     cx
+    pop     es
+    pop     di
+
+ret 
+
+minPointShape:
+    
+
+    push    di
+    push    es
+    push    cx    
+
+    mov     cx,80
+    mov     ah,0
+    mul     cx
+    mov     bh,0
+    add     ax,bx
+    shl     ax,1
+    mov     di,ax           ; selecting location on screen
+    mov     ax,0xB800
+    mov     es,ax
+    CLD
+    mov     ax,0x36C9       ; just printing shape afterwards
+    STOSw 
+    mov     ax,0x36CD
+    STOSw 
+    mov     ax,0x36BB
+    STOSw 
+    add     di,154
+    mov     ax,0x36B0
+    STOSw 
+    mov     ax,0x36B2
+    STOSw 
+    mov     ax,0x36B0
+    STOSw 
+    add     di,154
+    mov     ax,0x36C8
+    STOSw
+    mov     ax,0x36CD
+    STOSw 
+    mov     ax,0x36BC
     STOSw
 
     pop     cx
@@ -346,27 +591,71 @@ loadGamePage:
     call    renderScoreNTime
     call    renderCatcher
     ; maybe a loop to call it again and again
-    mov     al,4 ; row can be randomly selected using random function
-    mov     bl,4 ; column selected 
+    mov     al,4 ; row 
+    mov     bl,4 ; column can be randomly selected using random function
     call    holdMyTnt
     
-    mov     al,7 ; row can be randomly selected using random function
-    mov     bl,140 ; column selected 
+    mov     al,8 ; row 
+    mov     bl,34 ; column can be randomly selected using random function
     call    holdMyTnt
 
-    mov     al,21 ; row can be randomly selected using random function
-    mov     bl,60 ; column selected 
+    mov     al,12 ; row 
+    mov     bl,64 ; column can be randomly selected using random function
     call    holdMyTnt
+    
+    
+    mov     al,12 ; row 
+    mov     bl,13 ; column can be randomly selected using random function
+    call    maxPointShape
 
-    ; maybe end the loop here
+    mov     al,16 ; row 
+    mov     bl,43; column can be randomly selected using random function
+    call    maxPointShape
+
+    mov     al,20 ; row 
+    mov     bl,70 ; column can be randomly selected using random function
+    call    maxPointShape
+
+    mov     al,9 ; row 
+    mov     bl,4; column can be randomly selected using random function
+    call    midPointShape
+
+    mov     al,13 ; row 
+    mov     bl,34; column can be randomly selected using random function
+    call    midPointShape
+
+    mov     al,17 ; row 
+    mov     bl,64; column can be randomly selected using random function
+    call    midPointShape
+
+    mov     al,6 ; row 
+    mov     bl,13; column can be randomly selected using random function
+    call    minPointShape
+
+    mov     al,10 ; row 
+    mov     bl,43; column can be randomly selected using random function
+    call    minPointShape
+
+    mov     al,14 ; row 
+    mov     bl,70; column can be randomly selected using random function
+    call    minPointShape
 
     ret
 waitAWhile
-    mov     cx, 2Dh
-    mov     dx, 0xC0C6
+    push    cx
+    push    dx 
+    push    ax
+
+    mov     cx, 0x20
+    mov     dx, 0x4240
     mov     al,0
     mov     ah, 86h
     int     15h
+
+    pop     ax
+    pop     dx
+    pop     cx
+
 ret
 start:
     call    loadMainMenu
