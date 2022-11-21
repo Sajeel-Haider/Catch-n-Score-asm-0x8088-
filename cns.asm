0305baa950aac2943a90bcb14f61a46c1ff4d28b
AhmadShykh <52326090+AhmadShykh@users.noreply.github.com>
2022-11-21 13:33:53 +0500

    Timer Added
    chal dafa hou

--- cns.asm
+++ cns.asm
@@ -22,7 +22,91 @@ text4: db' /  /  |    ||_| |_/  /  |  _  ||  ||  |  ||  |\__  |/  /  |  O  ||   
 text5: db'/   \_ |  _ |  | |/   \_ |  |  ||__||  |  ||__|/  \ /   \_ |     ||    \ |   [_ ',0
 text6: db'\     ||  | |  | |\     ||  |  |    |  |  |    \    \     ||     ||  .  \|     |',0
 text7: db' \____||__|_|  |_| \____||__|__|    |__|__|     \___|\____| \___/ |__|\_||_____|',0
-                                                                                        
+
+
+
+tickcount: dw 0 
+tickseconds: dw 0 
+tickmins: dw 0 
+; subroutine to print a number at top left of screen 
+; takes the number to be printed as its parameter 
+printnum: push bp 
+    mov bp, sp 
+    push es 
+    push ax 
+    push bx 
+    push cx 
+    push dx 
+    push di 
+    mov ax, 0xb800 
+    mov es, ax ; point es to video base 
+    mov ax, [bp+4] ; load number in ax 
+    mov bx, 10 ; use base 10 for division 
+    mov cx, 0 ; initialize count of digits 
+    nextdigit: mov dx, 0 ; zero upper half of dividend 
+    div bx ; divide by 10 
+    add dl, 0x30 ; convert digit into ascii value 
+    push dx ; save ascii value on stack 
+    inc cx ; increment count of values 
+    cmp ax, 0 ; is the quotient zero 
+    jnz nextdigit ; if no divide it again 
+    mov di, [bp+6] ; point di to 70th column 
+    nextpos: pop dx ; remove a digit from the stack 
+    mov dh, 0x67 ; use normal attribute 
+    mov [es:di], dx ; print char on screen 
+    add di, 2 ; move to next screen location 
+    loop nextpos ; repeat for all digits on stack 
+    pop di 
+    pop dx 
+    pop cx 
+    pop bx 
+    pop ax
+    pop es 
+    pop bp 
+    ret 4
+; timer interrupt service routine 
+timer: push ax
+    push es
+    inc word [tickcount]; increment tick count
+
+    mov ax,0xB800
+    mov es, ax
+
+    mov word ax, [tickcount]
+    mov byte bl, 5
+    div bl
+    mov [tickseconds],al
+    mov ax, 176
+    push ax
+    push word [tickseconds]
+    call printnum ; print tick count
+    mov ax, [tickseconds] 
+    cmp ax, 60
+    jne dontIncMin
+    mov word [tickseconds],0
+    
+    mov di,178
+    mov word [es:di],0x6720
+    mov word [tickcount],0
+    mov ax,[tickmins]
+    inc ax
+    mov [tickmins],ax
+
+
+    dontIncMin:
+        mov di,174
+        mov word [es:di], 0x673A
+        mov ax, 172
+        push ax
+        push word [tickmins]
+        call printnum ; print tick count
+        mov al, 0x20 
+        out 0x20, al ; end of interrupt
+        pop es 
+        pop ax 
+    iret ; return from interrupt 
+  
+
 
 clearScreen:
     mov     ax,     00h   ;x co-ordinate
@@ -967,12 +1051,33 @@ waitAWhile
     pop     dx
     pop     cx
     ret
+hookTimer:
+    push ax 
+    push es
+    xor ax, ax 
+    mov es, ax ; point es to IVT base 
+    cli ; disable interrupts 
+    mov word [es:8*4], timer; store offset at n*4 
+    mov [es:8*4+2], cs ; store segment at n*4+2 
+    sti
+
+    pop es 
+    pop ax
+
+
+    ret 
+
+
+
 start:
+    
     ;call    loadMainMenu
     ;call    waitAWhile
     ;call    loadInstructionsPage
     ;call    waitAWhile
+    call    hookTimer
     call    loadGamePage
+    
     ;call    waitAWhile
     ;call    loadEndPage
     
